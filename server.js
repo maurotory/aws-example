cd3f2f8ac8ad7b06352069d81a1b9d17da19f81d
mauri <maurici.toribio@gmail.com>
2021-03-19 19:25:07 +0100

    texto modificado

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("Hello World from Express")
+    res.send("<h1>Express con HTML!</h1>")
 })
 
 
